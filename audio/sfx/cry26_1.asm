1becfd280a1686ef1ba5177ed6d335eabb6939c5
Pokeglitch <pokeglitchx@gmail.com>
2015-11-10 22:25:01 -0500

    Figured out the volume problem.  Keep the lower nybble as 1 and everything will work properly.  Has to do with the sound hardware register flags.

--- audio/sfx/cry26_1.asm
+++ audio/sfx/cry26_1.asm
@@ -1,29 +1,29 @@
 SFX_Cry26_1_Data::
 SFX_Cry26_1_Ch1: ; 8987 (2::4987)
 	dutycycle 0
-	unknownsfx0x20 2, 209, $8d, 7
-	unknownsfx0x20 2, 209, $91, 7
-	unknownsfx0x20 2, 209, $ad, 7
-	unknownsfx0x20 2, 209, $b4, 7
-	unknownsfx0x20 1, 209, $b8, 7
+	unknownsfx0x20 2, $F1, $8d, 7
+	unknownsfx0x20 2, $F1, $91, 7
+	unknownsfx0x20 2, $F1, $ad, 7
+	unknownsfx0x20 2, $F1, $b4, 7
+	unknownsfx0x20 1, $F1, $b8, 7
 	endchannel
 
 
 ;SFX_Cry26_1_Data::
 SFX_Cry26_1_Ch2: ; 8996 (2::4996)
 	dutycycle 16
-	unknownsfx0x20 2, 177, $6d, 7
-	unknownsfx0x20 2, 177, $71, 7
-	unknownsfx0x20 2, 177, $8d, 7
-	unknownsfx0x20 2, 177, $94, 7
-	unknownsfx0x20 1, 177, $98, 7
+	unknownsfx0x20 2, $C1, $6d, 7
+	unknownsfx0x20 2, $C1, $71, 7
+	unknownsfx0x20 2, $C1, $8d, 7
+	unknownsfx0x20 2, $C1, $94, 7
+	unknownsfx0x20 1, $C1, $98, 7
 	endchannel
 
 ;SFX_Cry26_1_Data::
 SFX_Cry26_1_Ch3: ; 89a5 (2::49a5)
-	unknownnoise0x20 2, 177, $1
-	unknownnoise0x20 2, 177, $19
-	unknownnoise0x20 2, 177, $18
-	unknownnoise0x20 2, 177, $10
-	unknownnoise0x20 1, 177, $18
+	unknownnoise0x20 2, $B1, $1
+	unknownnoise0x20 2, $B1, $19
+	unknownnoise0x20 2, $C1, $18
+	unknownnoise0x20 2, $B1, $10
+	unknownnoise0x20 1, $C1, $18
 	endchannel
